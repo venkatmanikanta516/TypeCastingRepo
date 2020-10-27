3534d357ef05b924dcc3fa3b0628ad4b4f13732a
venkatmanikanta516 <venkat.purushothapu@gmail.com>
2020-10-27 09:32:50 +0530

    Commited new java project

---
+++ TypeCastingProject/src/com/Mjava/TypeCastingDemo.java
@@ -0,0 +1,16 @@
+package com.Mjava;
+
+public class TypeCastingDemo {
+
+	public static void main(String[] args) {
+		 int i=10;
+		 long j=i;
+		 double d=j;
+		 
+		 System.out.println("Integer value : "+i);
+		 System.out.println("Long  value : "+j);
+		 System.out.println("Double  value : "+d);
+
+	}
+
+}
